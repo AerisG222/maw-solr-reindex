e0c7f0adcf6a2ddb0e63bf4b4348d91b4c832e3b
Mike Morano <mmorano@mikeandwan.us>
2020-06-06 08:08:11 -0400

    define github action

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # maw-solr-reindex
+
+![](https://github.com/AerisG222/maw-solr-reindex/workflows/CI/badge.svg)
+
 Container for kicking off the solr reindex for mikeandwan.us
---
+++ _prod_build_and_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+IID_FILE=image.iid
+TAG=$(date +%Y%m%d%H%M%S)
+
+buildah bud -f Containerfile -t maw-solr-reindex --iidfile "${IID_FILE}"
+IID=$(cat "${IID_FILE}")
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr-reindex:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-solr-reindex:latest
