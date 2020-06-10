095091c02adf9c947354a137f7f3c86a3a3bb64b
Mike Morano <mmorano@mikeandwan.us>
2020-06-09 20:40:59 -0400

    poor mans cron

--- solr-reindex.sh
+++ solr-reindex.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
-# delete all documents
-curl -X POST -H 'Content-Type: application/json' --data-binary '{"delete":{"query":"*:*" }}' http://localhost:8983/solr/multimedia-categories/update/?commit=true
+# poor mans cron
+while true; do
+    sleep 55m
 
-# kick off re-index
-curl http://localhost:8983/solr/multimedia-categories/dataimport?command=full-import
+    echo '*** checking if we need to run reindex ***'
+
+    hour=$(date +"%H")
+
+    if [ "${hour}" = "03" ]; then
+        echo '*** executing reindex ***'
+
+        # delete all documents
+        curl -X POST -H 'Content-Type: application/json' --data-binary '{"delete":{"query":"*:*" }}' http://localhost:8983/solr/multimedia-categories/update/?commit=true
+
+        # kick off re-index
+        curl http://localhost:8983/solr/multimedia-categories/dataimport?command=full-import
+    fi
+done
